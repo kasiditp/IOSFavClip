a004b7f6db322504bb50253f79fda754a99aeb15
Kasidit Phoncharoen <kasidit.p@ku.th>
2017-03-15 10:31:35 +0700

    first commit

--- MyFavClip/VideoVC.swift
+++ MyFavClip/VideoVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  VideoVC.swift
 //  MyFavClip
 //
 //  Created by Kasidit Phoncharoen on 2/16/2560 BE.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class VideoVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
